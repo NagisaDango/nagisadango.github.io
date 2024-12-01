57838d0e126c45e2ead96a9a0a37a559c4f2d40b
zsj <Jack_zhang.sj@outlook.com>
2024-11-30 18:59:10 -0800

    Site updated: 2024-11-30 18:59:09

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1733006149644";
+const VERSION = "1733021948717";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
